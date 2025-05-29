ef33f2b22f41b36aad93fe85e1b50c7d2a7c0097
Mlag123 <disw@list.ru>
2025-05-30 03:43:11 +0700

    sync

--- src/main/java/Core/JsonSQL.java
+++ src/main/java/Core/JsonSQL.java
@@ -1,6 +1,12 @@
 package Core;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
       Хранение должно быть такое.
@@ -11,31 +17,40 @@ import com.google.gson.Gson;
  */
 public class JsonSQL<T> {
     private T _class;
-    private Gson gson = new Gson();
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final String DB_FILE = "./database/database.json";
+    private String path = "./database";
 
-    public JsonSQL(T object) {
+    public JsonSQL() throws IOException {
+
+        File a = new File(path);
+        if (!a.isDirectory()){
+            a.mkdir();
+        }
+        File b = new File(DB_FILE);
+
+        if(!b.isFile()){
+            a.createNewFile();
+        }
 
 
-           String json = gson.toJson(object);// fixme важно проверить, что T - действительно является классом.
+    }
 
+    public void saveBase(List<T> base){
+        try (Writer writer = new FileWriter(DB_FILE)){
 
-        System.out.println(json);
+            gson.toJson(base,writer);
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
 
+    public List<T> loadBase(){
+    
     }
 
 
 }
 
 
-class User{
 
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public User(String name) {
-        this.name = name;
-    }
-}
